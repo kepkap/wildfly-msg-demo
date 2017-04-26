211434966dfcd983ce804bb4008bbd49933da21a
kepkap <kepkap.dev@gmail.com>
2017-04-26 10:18:45 +0300

    messing around with wildfly setup

---
+++ src/main/java/pro/wildflymsg/demo/SomeRestController.java
@@ -0,0 +1,14 @@
+package pro.wildflymsg.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by kepkap
+ * on 26/04/2017.
+ */
+@ApplicationPath("/")
+public class SomeRestController extends Application {
+
+
+}
